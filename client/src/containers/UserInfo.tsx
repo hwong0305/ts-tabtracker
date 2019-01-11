ff950752c5916dbc89c4e0569377c606ad934be5
hwong0305 <hwong.0305@gmail.com>
2019-01-10 23:19:29 -0800

    started creating userinfo view

---
+++ client/src/containers/UserInfo.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './styles/UserInfo.css';
+
+class UserInfo extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <p>Hello there</p>
+            </div>
+        );
+    }
+}
+
+export default UserInfo;
